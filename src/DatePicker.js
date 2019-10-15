0c26bc3520b6d4afa01cfdaf5434346c9d94ece3
James Sorakris <sorakris@bluebik.com>
2019-10-15 18:40:11 +0700

    satisfied with the code todo:handle case

--- src/DatePicker.js
+++ src/DatePicker.js
@@ -1,19 +1,78 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {Picker, Text, TouchableOpacity, View} from 'react-native';
+import { Picker, Text, TouchableOpacity, View } from 'react-native';
 import RBSheet from 'react-native-raw-bottom-sheet';
 import styles from './styles';
 
+const monthList = [
+  'January',
+  'Febuary',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
 class DatePicker extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
-    const {selectedHour, selectedMinute} = props;
-    this.state = {selectedHour, selectedMinute};
+    const { selectedHour, selectedMinute } = props;
+    this.state = { selectedHour, selectedMinute };
   }
 
+  getYearItems = () => {
+    const items = [];
+    const { minYear, maxYear, yearInterval, yearUnit } = this.props;
+    const interval = maxYear / yearInterval;
+    for (let i = 1; i <= interval; i++) {
+      const value = yearList[i - 1 * yearInterval];
+      console.log('value', value);
+      const item = (
+          <Picker.Item key={value} value={value}
+                       label={value + yearUnit}/>
+      );
+      items.push(item);
+    }
+    return items;
+  };
+  getMonthItems = () => {
+    const items = [];
+    const { maxMonth, monthInterval, monthUnit } = this.props;
+    const interval = maxMonth / monthInterval;
+    for (let i = 1; i <= interval; i++) {
+      const value = monthList[i - 1 * monthInterval];
+      console.log('value', value);
+      const item = (
+          <Picker.Item key={value} value={value}
+                       label={value + monthUnit}/>
+      );
+      items.push(item);
+    }
+    return items;
+  };
+  getDayItems = () => {
+    const items = [];
+    const { maxDay, dayInterval, dayUnit } = this.props;
+    const interval = maxDay / dayInterval;
+    for (let i = 1; i <= interval; i++) {
+      const value = `${i * dayInterval}`;
+      const item = (
+          <Picker.Item key={value} value={value}
+                       label={value + dayUnit}/>
+      );
+      items.push(item);
+    }
+    return items;
+  };
   getHourItems = () => {
     const items = [];
-    const {maxHour, hourInterval, hourUnit} = this.props;
+    const { maxHour, hourInterval, hourUnit } = this.props;
     const interval = maxHour / hourInterval;
     for (let i = 0; i <= interval; i++) {
       const value = `${i * hourInterval}`;
@@ -30,7 +89,7 @@ class DatePicker extends Component {
   getMinuteItems = () => {
     console.log('getMinuteItems');
     const items = [];
-    const {maxMinute, minuteInterval, minuteUnit} = this.props;
+    const { maxMinute, minuteInterval, minuteUnit } = this.props;
     const interval = maxMinute / minuteInterval;
     for (let i = 0; i <= interval; i++) {
       const value = i * minuteInterval;
@@ -44,12 +103,14 @@ class DatePicker extends Component {
       );
       items.push(item);
     }
-    const extra = this.props.extraMinuteItems(this.state.selectedHour,
-        this.state.selectedMinute);
-    if (extra) {
-      extra.map(minuteItem => {
-        items.push(minuteItem);
-      });
+    if (this.props.extraMinuteItems) {
+      const extra = this.props.extraMinuteItems(this.state.selectedHour,
+          this.state.selectedMinute);
+      if (extra) {
+        extra.map(minuteItem => {
+          items.push(minuteItem);
+        });
+      }
     }
 
     return items;
@@ -57,19 +118,19 @@ class DatePicker extends Component {
 
   onValueChange = (selectedHour, selectedMinute) => {
     let items = [];
-    this.setState({selectedHour, selectedMinute});
+    this.setState({ selectedHour, selectedMinute });
   };
 
   onCancel = () => {
     if (typeof this.props.onCancel === 'function') {
-      const {selectedHour, selectedMinute} = this.state;
+      const { selectedHour, selectedMinute } = this.state;
       this.props.onCancel(selectedHour, selectedMinute);
     }
   };
 
   onConfirm = () => {
     if (typeof this.props.onConfirm === 'function') {
-      const {selectedHour, selectedMinute} = this.state;
+      const { selectedHour, selectedMinute } = this.state;
       this.props.onConfirm(selectedHour, selectedMinute);
     }
   };
@@ -83,7 +144,7 @@ class DatePicker extends Component {
   };
 
   renderHeader = () => {
-    const {textCancel, textConfirm, textTitle} = this.props;
+    const { textCancel, textConfirm, textTitle } = this.props;
     return (
         <View style={styles.header}>
           <TouchableOpacity onPress={this.onCancel} style={styles.buttonAction}>
@@ -95,7 +156,7 @@ class DatePicker extends Component {
           <View style={styles.buttonAction}>
             <Text style={[
               styles.buttonText,
-              {color: 'black', fontWeight: '500'}]}>{textTitle}</Text>
+              { color: 'black', fontWeight: '500' }]}>{textTitle}</Text>
           </View>}
           <TouchableOpacity onPress={this.onConfirm}
                             style={styles.buttonAction}>
@@ -106,37 +167,45 @@ class DatePicker extends Component {
   };
 
   renderBody = () => {
-    const {selectedHour, selectedMinute} = this.state;
+    const { selectedDay, selectedMonth, sleectedYear } = this.state;
 
     return (
         <View style={styles.body}>
           <Picker
-              selectedValue={selectedHour}
+              selectedValue={selectedDay}
               style={styles.picker}
               itemStyle={this.props.itemStyle}
               onValueChange={itemValue =>
-                  this.onValueChange(itemValue, selectedMinute)
+                  this.onValueChange(itemValue, selectedDay)
               }
           >
-            {this.getHourItems()}
+            {this.getDayItems()}
           </Picker>
-          <Text style={styles.separator}>:</Text>
           <Picker
-              selectedValue={selectedMinute}
+              selectedValue={selectedMonth}
               style={styles.picker}
               itemStyle={this.props.itemStyle}
               onValueChange={itemValue =>
-                  this.onValueChange(selectedHour, itemValue)
+                  this.onValueChange(itemValue, selectedMonth)
               }
           >
-            {/* {this.props.getMinuteItems?this.props.getMinuteItems():this.getMinuteItems()} */}
-            {this.getMinuteItems()}
+            {this.getMonthItems()}
+          </Picker>
+          <Picker
+              selectedValue={selectedYear}
+              style={styles.picker}
+              itemStyle={this.props.itemStyle}
+              onValueChange={itemValue =>
+                  this.onValueChange(itemValue, selectedYear)
+              }
+          >
+            {this.getYearItems()}
           </Picker>
         </View>
     );
   };
 
-  render() {
+  render () {
     return (
         <RBSheet
             ref={ref => {
@@ -151,35 +220,59 @@ class DatePicker extends Component {
 }
 
 DatePicker.propTypes = {
+  minYear: PropTypes.number,
+  maxDay: PropTypes.number,
   maxHour: PropTypes.number,
+  maxMonth: PropTypes.number,
   maxMinute: PropTypes.number,
+  maxYear: PropTypes.number,
   hourInterval: PropTypes.number,
   minuteInterval: PropTypes.number,
+  dayInterval: PropTypes.number,
+  monthInterval: PropTypes.number,
   hourUnit: PropTypes.string,
   minuteUnit: PropTypes.string,
+  dayUnit: PropTypes.string,
+  monthUnit: PropTypes.string,
   selectedHour: PropTypes.string,
   selectedMinute: PropTypes.string,
+  selectedDay: PropTypes.string,
+  selectedYear: PropTypes.string,
   itemStyle: PropTypes.object,
   textCancel: PropTypes.string,
   textConfirm: PropTypes.string,
   textTitle: PropTypes.string,
   onCancel: PropTypes.func,
-  onConfirm: PropTypes.func,
+  onConfirm: PropTypes.func
 };
 
 DatePicker.defaultProps = {
+  minYear: 1,
+  maxDay: 31,
   maxHour: 23,
   maxMinute: 59,
+  maxMonth: 12,
+  maxYear: 9999,
+  monthList: monthList,
   hourInterval: 1,
   minuteInterval: 1,
+  monthInterval: 1,
+  yearInterval: 1,
+  dayInterval: 1,
   hourUnit: '',
   minuteUnit: '',
+  dayUnit: '',
+  monthUnit: '',
+  yearUnit: '',
   selectedHour: '0',
   selectedMinute: '00',
+  selectedDay: '1',
+  selectedMonth: '1',
+  selectedYear: '2000',
   itemStyle: {},
   textCancel: 'Cancel',
   textConfirm: ' Done ',
-  textTitle: null,
+  textTitle: null
 };
 
 export default DatePicker;
--- src/TimePicker.js
+++ src/TimePicker.js
@@ -1,25 +1,26 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { View, TouchableOpacity, Text, Picker } from "react-native";
-import RBSheet from "react-native-raw-bottom-sheet";
-import styles from "./styles";
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {Picker, Text, TouchableOpacity, View} from 'react-native';
+import RBSheet from 'react-native-raw-bottom-sheet';
+import styles from './styles';
 
 class TimePicker extends Component {
   constructor(props) {
     super(props);
-    const { selectedHour, selectedMinute } = props;
-    this.state = { selectedHour, selectedMinute };
+    const {selectedHour, selectedMinute} = props;
+    this.state = {selectedHour, selectedMinute};
   }
 
   getHourItems = () => {
     const items = [];
-    const { maxHour, hourInterval, hourUnit } = this.props;
+    const {maxHour, hourInterval, hourUnit} = this.props;
     const interval = maxHour / hourInterval;
     for (let i = 0; i <= interval; i++) {
       const value = `${i * hourInterval}`;
       const new_value = value < 10 ? `0${value}` : `${value}`;
       const item = (
-        <Picker.Item key={value} value={new_value} label={new_value + hourUnit} />
+          <Picker.Item key={value} value={new_value}
+                       label={new_value + hourUnit}/>
       );
       items.push(item);
     }
@@ -27,47 +28,47 @@ class TimePicker extends Component {
   };
 
   getMinuteItems = () => {
-    console.log('getMinuteItems')
     const items = [];
-    const { maxMinute, minuteInterval, minuteUnit } = this.props;
+    const {maxMinute, minuteInterval, minuteUnit} = this.props;
     const interval = maxMinute / minuteInterval;
     for (let i = 0; i <= interval; i++) {
       const value = i * minuteInterval;
       const new_value = value < 10 ? `0${value}` : `${value}`;
       const item = (
-        <Picker.Item
-          key={value}
-          value={new_value}
-          label={new_value + minuteUnit}
-        />
+          <Picker.Item
+              key={value}
+              value={new_value}
+              label={new_value + minuteUnit}
+          />
       );
       items.push(item);
     }
-    const extra = this.props.extraMinuteItems(this.state.selectedHour,this.state.selectedMinute)
-    if(extra){
-      extra.map(minuteItem=>{
-        items.push(minuteItem)
-      })
+    const extra = this.props.extraMinuteItems(this.state.selectedHour,
+        this.state.selectedMinute);
+    if (extra) {
+      extra.map(minuteItem => {
+        items.push(minuteItem);
+      });
     }
-    
+
     return items;
   };
 
   onValueChange = (selectedHour, selectedMinute) => {
-    let items = []
-    this.setState({ selectedHour, selectedMinute });
+    let items = [];
+    this.setState({selectedHour, selectedMinute});
   };
 
   onCancel = () => {
-    if (typeof this.props.onCancel === "function") {
-      const { selectedHour, selectedMinute } = this.state;
+    if (typeof this.props.onCancel === 'function') {
+      const {selectedHour, selectedMinute} = this.state;
       this.props.onCancel(selectedHour, selectedMinute);
     }
   };
 
   onConfirm = () => {
-    if (typeof this.props.onConfirm === "function") {
-      const { selectedHour, selectedMinute } = this.state;
+    if (typeof this.props.onConfirm === 'function') {
+      const {selectedHour, selectedMinute} = this.state;
       this.props.onConfirm(selectedHour, selectedMinute);
     }
   };
@@ -81,66 +82,69 @@ class TimePicker extends Component {
   };
 
   renderHeader = () => {
-    const { textCancel, textConfirm, textTitle } = this.props;
+    const {textCancel, textConfirm, textTitle} = this.props;
     return (
-      <View style={styles.header}>
-        <TouchableOpacity onPress={this.onCancel} style={styles.buttonAction}>
-          <Text style={[styles.buttonText]}>
-            {textCancel}
-          </Text>
-        </TouchableOpacity>
-        {textTitle &&
+        <View style={styles.header}>
+          <TouchableOpacity onPress={this.onCancel} style={styles.buttonAction}>
+            <Text style={[styles.buttonText]}>
+              {textCancel}
+            </Text>
+          </TouchableOpacity>
+          {textTitle &&
           <View style={styles.buttonAction}>
-            <Text style={[styles.buttonText, {color:'black', fontWeight:'500'}]}>{textTitle}</Text>
+            <Text style={[
+              styles.buttonText,
+              {color: 'black', fontWeight: '500'}]}>{textTitle}</Text>
           </View>}
-        <TouchableOpacity onPress={this.onConfirm} style={styles.buttonAction}>
-          <Text style={styles.buttonText}>{textConfirm}</Text>
-        </TouchableOpacity>
-      </View>
+          <TouchableOpacity onPress={this.onConfirm}
+                            style={styles.buttonAction}>
+            <Text style={styles.buttonText}>{textConfirm}</Text>
+          </TouchableOpacity>
+        </View>
     );
   };
 
   renderBody = () => {
-    const { selectedHour, selectedMinute } = this.state;
-    
+    const {selectedHour, selectedMinute} = this.state;
+
     return (
-      <View style={styles.body}>
-        <Picker
-          selectedValue={selectedHour}
-          style={styles.picker}
-          itemStyle={this.props.itemStyle}
-          onValueChange={itemValue =>
-            this.onValueChange(itemValue, selectedMinute)
-          }
-        >
-          {this.getHourItems()}
-        </Picker>
-        <Text style={styles.separator}>:</Text>
-        <Picker
-          selectedValue={selectedMinute}
-          style={styles.picker}
-          itemStyle={this.props.itemStyle}
-          onValueChange={itemValue =>
-            this.onValueChange(selectedHour, itemValue)
-          }
-        >
-          {/* {this.props.getMinuteItems?this.props.getMinuteItems():this.getMinuteItems()} */}
-          {this.getMinuteItems()}
-        </Picker>
-      </View>
+        <View style={styles.body}>
+          <Picker
+              selectedValue={selectedHour}
+              style={styles.picker}
+              itemStyle={this.props.itemStyle}
+              onValueChange={itemValue =>
+                  this.onValueChange(itemValue, selectedMinute)
+              }
+          >
+            {this.getHourItems()}
+          </Picker>
+          <Text style={styles.separator}>:</Text>
+          <Picker
+              selectedValue={selectedMinute}
+              style={styles.picker}
+              itemStyle={this.props.itemStyle}
+              onValueChange={itemValue =>
+                  this.onValueChange(selectedHour, itemValue)
+              }
+          >
+            {/* {this.props.getMinuteItems?this.props.getMinuteItems():this.getMinuteItems()} */}
+            {this.getMinuteItems()}
+          </Picker>
+        </View>
     );
   };
 
   render() {
     return (
-      <RBSheet
-        ref={ref => {
-          this.RBSheet = ref;
-        }}
-      >
-        {this.renderHeader()}
-        {this.renderBody()}
-      </RBSheet>
+        <RBSheet
+            ref={ref => {
+              this.RBSheet = ref;
+            }}
+        >
+          {this.renderHeader()}
+          {this.renderBody()}
+        </RBSheet>
     );
   }
 }
@@ -159,7 +163,7 @@ TimePicker.propTypes = {
   textConfirm: PropTypes.string,
   textTitle: PropTypes.string,
   onCancel: PropTypes.func,
-  onConfirm: PropTypes.func
+  onConfirm: PropTypes.func,
 };
 
 TimePicker.defaultProps = {
@@ -167,14 +171,14 @@ TimePicker.defaultProps = {
   maxMinute: 59,
   hourInterval: 1,
   minuteInterval: 1,
-  hourUnit: "",
-  minuteUnit: "",
-  selectedHour: "0",
-  selectedMinute: "00",
+  hourUnit: '',
+  minuteUnit: '',
+  selectedHour: '0',
+  selectedMinute: '00',
   itemStyle: {},
-  textCancel: "Cancel",
-  textConfirm: " Done ",
-  textTitle: null
+  textCancel: 'Cancel',
+  textConfirm: ' Done ',
+  textTitle: null,
 };
 
 export default TimePicker;
